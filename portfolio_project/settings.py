251dc4e2c6d4b964da34cdbebf86d70d84daca27
Olatunde Emmanuel <mohbohlahji@hotmail.com>
2025-06-24 08:55:06 +0100

    Deployment settings

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,10 +69,7 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
